a7e81e67ce61e66652f66b5c483ccca35e694a45
zhiyong.guo <zhiyong.guo@bioknow.net>
2020-10-09 15:03:56 +0800

    创建一个博客项目

---
+++ base-provider/src/main/java/com/dazhi/baseprovider/BaseProviderApplication.java
@@ -0,0 +1,13 @@
+package com.dazhi.baseprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseProviderApplication.class, args);
+    }
+
+}
---
+++ base-provider/src/test/java/com/dazhi/baseprovider/BaseProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazhi.baseprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
