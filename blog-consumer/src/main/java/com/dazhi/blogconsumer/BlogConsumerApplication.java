f9af79a55dcb00b979c964dfe838d0a563db8996
zhiyong.guo <zhiyong.guo@bioknow.net>
2020-10-09 18:28:30 +0800

    创建一个博客项目

---
+++ blog-consumer/src/main/java/com/dazhi/blogconsumer/BlogConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dazhi.blogconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogConsumerApplication.class, args);
+    }
+
+}
---
+++ blog-consumer/src/test/java/com/dazhi/blogconsumer/BlogConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazhi.blogconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
