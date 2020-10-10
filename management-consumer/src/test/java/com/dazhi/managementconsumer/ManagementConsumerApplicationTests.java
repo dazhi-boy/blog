e09ece765a9b9c78b1344ec2efd77eb528c156bc
zhiyong.guo <zhiyong.guo@bioknow.net>
2020-10-10 18:30:09 +0800

    创建一个博客项目

---
+++ management-consumer/src/test/java/com/dazhi/managementconsumer/ManagementConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazhi.managementconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
