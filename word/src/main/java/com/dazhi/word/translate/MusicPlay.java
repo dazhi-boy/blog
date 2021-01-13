c19c782ec15f73b82393a50d37890638dcd28c72
zhiyong.guo <zhiyong.guo@bioknow.net>
2021-01-13 18:51:13 +0800

    创建一个博客项目

--- word/src/main/java/com/dazhi/word/translate/MusicPlay.java
+++ word/src/main/java/com/dazhi/word/translate/MusicPlay.java
@@ -1,11 +1,20 @@
 package com.dazhi.word.translate;
 
+import com.dazhi.word.util.HttpUtil;
+
 public class MusicPlay {
     public static void main(String[] args) {
+
+        String res = HttpUtil.getHttp("https://danci.911cha.com/lesson_173.html");
+        System.out.println(res);
+
+
+
+
         //get words url
-        for (int i = 0; i < GLOBAL.words.size(); i++) {
+        /*for (int i = 0; i < GLOBAL.words.size(); i++) {
             GLOBAL.wordsUrls.add(GLOBAL.baseAPI + GLOBAL.words.get(i).getTerm());
-        }
+        }*/
 
         //download
         /*DownloadUtils downloadUtils = new DownloadUtils();
