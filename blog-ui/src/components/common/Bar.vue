51cb1129afd69a2c59bfee626fc63613b9abe488
zhiyong.guo <zhiyong.guo@bioknow.net>
2021-06-18 20:51:51 +0800

    修改页面

---
+++ blog-ui/src/components/common/Bar.vue
@@ -0,0 +1,19 @@
+<template>
+  <van-nav-bar title="标题" left-text="返回" left-arrow>
+      <van-icon name="search" slot="right" />
+  </van-nav-bar>
+</template>
+<script>
+export default {
+  name: 'Bar',
+  data () {
+    return {
+      active: 0
+    }
+  },
+  mounted: function () {}
+}
+</script>
+<style>
+
+</style>
